849cdec06a364477ff4b535dbdd6986aa416cf9e
diegonti <diegontidoc2000@gmail.com>
2022-12-29 21:54:31 +0100

    Copied H2 to HeH+ program

--- RHF parametrized/p2.py
+++ RHF parametrized/p2.py
@@ -5,24 +5,35 @@ Diego Ontiveros
 """
 
 import numpy as np
+from time import time
+to = time()
 
-def bie_index(ijkl):
-    """Takes string of the indices of the bielectronic integral and returns
+def print_title(text,before=15,after=15,separator="-",head=2,tail=1):
+    """Prints text in a title-style."""
+    separator = str(separator)
+    print("\n"*head,separator*before,text,separator*after,"\n"*tail)
+
+def bie_index(ijkl:str):
+    """Takes string of the "ijkl" indices of the bielectronic integral and returns
     the index of the bielectronic parametrized array of integrals it corresponds."""
     i,j,k,l =[int(t) for t in ijkl]
     sij = i+j if i!=j else i
     skl = k+l if k!=l else k
     sijkl = [sij,skl]
-    if len(set(ijkl)) <= 1: return 0
-    elif sorted(sijkl) == [1,2]: return 1
-    elif sorted(sijkl) == [1,3]: return 2
-    elif sorted(sijkl) == [2,3]: return 2
-    elif sorted(sijkl) == [3,3]: return 3
+    if len(set(ijkl)) <= 1: return 0              # (ii|ii) cases
+    elif sorted(sijkl) == [1,2]: return 1       # (ii|jj) cases
+    elif sorted(sijkl) == [1,3]: return 2       # (ii|ij) cases
+    elif sorted(sijkl) == [2,3]: return 2       # (jj|ij) cases
+    elif sorted(sijkl) == [3,3]: return 3       # (ij|ij) cases
     else: raise ValueError("Bielectronic indices nor valid.")
 
-def G_matrix(m,P):
-    """Computed bielectornic matrix."""
-
+def G_matrix(P):
+    """
+    Computes bielectornic matrix.\n
+    Parameters: `P` : m x m density matrix.\n
+    Returns: `G` : m x m bielectronic matrix.
+    """
+    m = len(P)
     G = np.zeros(shape=(m,m))
     for mu in range(m):
         for nu in range(m):
@@ -30,18 +41,22 @@ def G_matrix(m,P):
             for l in range(m):
                 for s in range(m):
 
+                    # Getting ijkl and index of the integral it corresponds
                     munusl = f"{mu+1}{nu+1}{s+1}{l+1}"
                     mulsnu = f"{mu+1}{l+1}{s+1}{nu+1}"
                     i1 = bie_index(munusl)    
                     i2 = bie_index(mulsnu)
-                    # print(munusl,mulsnu)
-                    # print(i1,i2)
 
                     G[mu,nu] += P[l,s]*(bielectronic[i1] - 0.5*bielectronic[i2])
-                    ######### Mirar bien las integrales! Ojo, usted!
+
     return G
 
 def P_matrix(m,C):
+    """
+    Computes Density matrix for the given coeffitients matrix.\n
+    Parameters: `C` : m x m coefitients matrix.\n
+    Returns: `P` : m x m density matrix.
+    """
     P = np.zeros(shape=(m,m))
     for l in range(m):
         for s in range(m):
@@ -52,39 +67,44 @@ def P_matrix(m,C):
 
 
 def converged(P0,Pt,eps):
-    """Determines if a step is converged with a certain tolerance (eps)."""
+    """
+    Determines if a SCF step is converged with a certain tolerance (eps).
+    `P0` : Old Density matrix. (m x m array)
+    `Pt` : New Density matrix. (m x m array)
+    `eps`: Tolerance value.
+    """
     m = len(Pt)
     diff = np.sqrt(np.sum((Pt-P0)**2)/m**2)
     
     if diff<=eps: return True
     else: return False
 
+######################### MAIN PROGRAM ####################
 
-
+print_title("Initial Molecular Integrals")
+# Inputs 
 N = 2               # Number of electrons
 m = 2               # Number of basis functions
 R = 1.4632          # He-H distance
-
 Za,Zb = 2,1         # Nuclear atomic charges
 za,zb = 2.0925,1.24 # Exponents
 eps = 1e-4          # SCF tolerance
 max_iter = 10       # Maximum number of iterations
-np.random.seed(3333)
 
 # Parametrized Hamiltonian and Overlapping matrices
-S = [1,0.6593]
-t = [0.7600,0.2365]
-v = [-1.2266,-0.5974,-0.6538]
 
 S = [1,0.4508]
-t = [0.21643,2.1643,0.7600]
-v = [[-4.1398,-1.1029,-1.2652],
-    [-0.6772,-0.4113,-1.226]]
-jk = [1.3072,0.7746,0.6057,0.4373,0.3118,0.1773]
+t = [2.1643,0.7600,0.1617]
+v = np.array(
+    [[-4.1398,-1.1029,-1.2652],
+    [-0.6772,-0.4113,-1.226]])
+vaa = v[0,0] + v[1,2]
+vab = v[0,1] + v[1,1]
+vbb = v[1,0] + v[0,2]
 
 S = np.array([S,S[::-1]])
-T = np.array([t,t[::-1]])
-V = np.array([[-1.2266,-0.5974],[-0.5974,-0.6538]])
+T = np.array([[2.1643,0.1617],[0.1617,0.7600]])
+V = np.array([[vaa,vab],[vab,vbb]])
 H = T+V
 
 print("\nOverlap Matrix:\n",S)
@@ -94,105 +114,84 @@ print("\nHamiltonian Matrix:\n",H)
 
 
 # Bielectronic integrals
-bielectronic = [0.7746,0.5697,0.4441,0.2970]
+bielectronic = [1.3072,0.7746,0.6057,0.4373,0.3118,0.1773]
 
 
-# Unitary matrix to build X
-Uij = 1/np.sqrt(2)
-U = np.array([[Uij,Uij],[Uij,-Uij]])
-
+# Transformation Matrix X from S so that XSX=1
 Seval,U = np.linalg.eigh(S)
 S12 = np.diag(Seval**-0.5)
 print("\nS12\n",S12)
 print("\nU\n",U)
 
-# S12 = np.linalg.inv(np.diag(np.sqrt(Seval)))
-# print("\nS12\n",S12)
-
-X = U@S12@U.T       #U@S12@U.T.conj()         ###### Probar U_dada y U_evec
-
-# Posible 
-# S12 = S[0,1]
-# X[0,0] = 1.0/np.sqrt(2.0*(1.0+S12))
-# X[1,0] = X[0,0]
-# X[0,1] = 1.0/np.sqrt(2.0*(1.0-S12))
-# X[1,1] = -X[0,1]
+X = U@S12      #U@S12@U.T.conj() also works   
 
 print("\nX\n",X)
 print("\nXSX\n",X.T@S@X)
 
 
-
-
-
-# Randomly initialized MO
-C = np.random.uniform(0,1,size=(m,m))
+# Guess density matrix (at zero)
 P0 = np.zeros(shape=(m,m))
-print("\nP0\n",P0)
+print("\nGuess P0\n",P0)
 
-print("\n\nEntering SCF loop...")
+
+# print("\n\n","-"*15,"Entering SCF loop","-"*15,"\n")
+print_title("Entering SCF loop")
+
 n_iterations = 0
 while True:
     n_iterations +=1
-    print(f"\nIternation {n_iterations}")
+    print_title(f"SCF Iternation: {n_iterations}",head=1,tail=0,before=10,after=0)
 
-    # Bielectronic Matrix
-    G = G_matrix(m,P0.copy())
-    print("G\n",G)
-
-    # Fock Matrix
-    F = H + G
-    print("F\n",F)
-
-    
-
-    # Tranformed Fock Matrix
-    Ft =  X.T.conj()@F@X
-    print("Ft\n",Ft)
-
-
-    # Orbital energies and coefs
-    e,Ct = np.linalg.eigh(Ft)
-    print("e\n",e)
-    C = X@Ct
-    print("C\n",C)
-
-    # Denisty Matrix
-    Pt = P_matrix(m,C)
-    # Pt = 2*C@C.T
-    print("P\n",Pt)
-    print("P0\n",P0)
-
+    # SCF step
+    G = G_matrix(P0.copy())     # Bielectronic Matrix
+    F = H + G                   # Fock Matrix
+    Ft =  X.T.conj()@F@X        # Tranformed Fock Matrix
+    e,Ct = np.linalg.eigh(Ft)   # Orbital Energies and transformed coefs
+    C = X@Ct                    # Orbital Coeffitients
+    Pt = P_matrix(m,C)          # Denisty Matrix
     Eelec = 0.5*np.sum(Pt*(H+F))
-
-    print("\nElcetronic Energy: \n", Eelec)
+    
+    # Printing current iteration results
+    print("\nBielectronic Matrix G:\n",G)
+    print("\nFock Matrix F:\n",F)
+    print("\nTransformed Fock Matrix Ft\n",Ft)
+    print("\nOrbital Energies e:\n",e)
+    print("\nOrbital Coeffitients C:\n",C)
+    print("\nDensity Matrix P\n",Pt)
+    print("\nElectronic Energy: \n", Eelec)
 
     # Convergence
     if converged(P0,Pt,eps):
-        print("\nConverged!")
-        print(C)
-        print(e)
+        print_title("Ending SCF loop. CONVERGED!")
 
-        # Functon to print info!
-        Vnn = 1/R
-        Eelec = Eelec
-        E = Eelec + Za*Zb/R
-        Mulliken = Pt@S
+        # Function to print info!
+
+        # Energy contributions
+        Vnn = Za*Zb/R           # Nuclear Repulsion
+        Eelec = Eelec           # Electronic Energy
+        E = Eelec + Vnn         # Total Energy
+        mulliken = Pt@S         # Mulliken Population Matrix
+
+        # Printing converged results
+        print("\nNumber of SCF iterations:",n_iterations)
         print("\nElectronic Energy: Eelec =",Eelec)
         print("Nuclear Repulsion:   Vnn = ",Vnn)
         print("Total energy:          E = ",Eelec+Vnn)
 
+        print("\nOrbital Energies e:\n",e)
+        print("\nOrbital Coeffitients C:\n",C)
+        print("\nMulliken Population Matrix:\n",mulliken)
+
         break
 
     if n_iterations >= max_iter:
-        print("\n Not converged!")
+        print_title("Ending SCF loop. NOT CONVERGED! :(")
         break
 
-    
-    P0 = Pt.copy()
+    P0 = Pt.copy()  # Updating Density Matrix
 
-
-
+tf = time()
+print(f"\nProcess finished in {tf-to:.5f}s.\n")
 
 
 
